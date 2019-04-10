d43b247d8167f86bc7d220d1026b12154b984eb5
lei <511281391@qq.com>
2019-04-10 15:40:56 +0800

    添加项目文件。

---
+++ WinFloatToolBar/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WinFloatToolBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGetGuid = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnDataSetNotNull = new System.Windows.Forms.Button();
+            this.btnGetDbHelper = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnGetColor = new System.Windows.Forms.Button();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.pbGetColor = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTo16 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGetColor)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetGuid
+            // 
+            this.btnGetGuid.Location = new System.Drawing.Point(0, 0);
+            this.btnGetGuid.Name = "btnGetGuid";
+            this.btnGetGuid.Size = new System.Drawing.Size(39, 23);
+            this.btnGetGuid.TabIndex = 0;
+            this.btnGetGuid.Text = "Guid";
+            this.btnGetGuid.UseVisualStyleBackColor = true;
+            this.btnGetGuid.Click += new System.EventHandler(this.btnGetGuid_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // btnDataSetNotNull
+            // 
+            this.btnDataSetNotNull.Location = new System.Drawing.Point(40, 0);
+            this.btnDataSetNotNull.Name = "btnDataSetNotNull";
+            this.btnDataSetNotNull.Size = new System.Drawing.Size(68, 23);
+            this.btnDataSetNotNull.TabIndex = 1;
+            this.btnDataSetNotNull.Text = "DsNotNull";
+            this.btnDataSetNotNull.UseVisualStyleBackColor = true;
+            this.btnDataSetNotNull.Click += new System.EventHandler(this.btnDataSetNotNull_Click);
+            // 
+            // btnGetDbHelper
+            // 
+            this.btnGetDbHelper.Location = new System.Drawing.Point(108, 0);
+            this.btnGetDbHelper.Name = "btnGetDbHelper";
+            this.btnGetDbHelper.Size = new System.Drawing.Size(62, 23);
+            this.btnGetDbHelper.TabIndex = 2;
+            this.btnGetDbHelper.Text = "DbHelper";
+            this.btnGetDbHelper.UseVisualStyleBackColor = true;
+            this.btnGetDbHelper.Click += new System.EventHandler(this.btnGetDbHelper_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(170, 0);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(17, 23);
+            this.btnShowAll.TabIndex = 3;
+            this.btnShowAll.Text = "↓";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnGetColor
+            // 
+            this.btnGetColor.Location = new System.Drawing.Point(0, 83);
+            this.btnGetColor.Name = "btnGetColor";
+            this.btnGetColor.Size = new System.Drawing.Size(45, 23);
+            this.btnGetColor.TabIndex = 4;
+            this.btnGetColor.Text = "color";
+            this.btnGetColor.UseVisualStyleBackColor = true;
+            this.btnGetColor.Click += new System.EventHandler(this.btnGetColor_Click);
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(51, 88);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(0, 12);
+            this.lblColor.TabIndex = 5;
+            // 
+            // pbGetColor
+            // 
+            this.pbGetColor.Location = new System.Drawing.Point(0, 0);
+            this.pbGetColor.Name = "pbGetColor";
+            this.pbGetColor.Size = new System.Drawing.Size(100, 100);
+            this.pbGetColor.TabIndex = 6;
+            this.pbGetColor.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pbGetColor);
+            this.panel1.Location = new System.Drawing.Point(0, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 100);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(49, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2, 2);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTo16
+            // 
+            this.btnTo16.Location = new System.Drawing.Point(0, 24);
+            this.btnTo16.Name = "btnTo16";
+            this.btnTo16.Size = new System.Drawing.Size(51, 23);
+            this.btnTo16.TabIndex = 8;
+            this.btnTo16.Text = "16进制";
+            this.btnTo16.UseVisualStyleBackColor = true;
+            this.btnTo16.Click += new System.EventHandler(this.btnTo16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 330);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnTo16);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.btnGetColor);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnGetDbHelper);
+            this.Controls.Add(this.btnDataSetNotNull);
+            this.Controls.Add(this.btnGetGuid);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGetColor)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetGuid;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnDataSetNotNull;
+        private System.Windows.Forms.Button btnGetDbHelper;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnGetColor;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.PictureBox pbGetColor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTo16;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinFloatToolBar/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFloatToolBar
+{
+    public partial class Form1 : Form
+    {
+        #region 变量
+        /// <summary>
+        /// 鼠标左键是否按下
+        /// </summary>
+        private bool mouseLeftDown = false;
+        /// <summary>
+        /// 鼠标移动位置变量
+        /// </summary>
+        Point mouseOff;
+        /// <summary>
+        /// 窗体是否为显示状态
+        /// </summary>
+        private bool formShow = true;
+        /// <summary>
+        /// 点击时鼠标在窗体的位置，此元素用来解决窗体左上角直接移动到鼠标位置
+        /// </summary>
+        Point pointMouseDown = new Point();
+        #endregion 变量
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Form event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //重新设置宽高，不然样式会变
+            //this.Width = 180;
+            this.Height = 23;
+
+            this.AllowDrop = true;//可接受拖拽文件至窗体
+
+            SetLocation();
+
+            #region 绑定相关事件
+            for (int i = 0; i < this.Controls.Count; i++)
+            {
+                if (this.Controls[i].Name.StartsWith("btn"))
+                {
+                    Button btnNow = this.Controls[i] as Button;
+                    if (btnNow != null)
+                    {
+                        btnNow.MouseDown += new MouseEventHandler(Form1_MouseDown);
+                        btnNow.MouseMove += new MouseEventHandler(dgMouseMove);
+                        btnNow.MouseUp += new MouseEventHandler(Form1_MouseUp);
+                        btnNow.MouseMove += new MouseEventHandler(Form1_MouseMove);
+                    }
+                }
+            }
+            #endregion 绑定相关事件
+        }
+        /// <summary>
+        /// 鼠标移动事件代理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgMouseMove(object sender, EventArgs e)
+        {
+            setFormMove();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseLeftDown = true;
+                Point formPoint = this.PointToClient(Control.MousePosition);
+                pointMouseDown.X = formPoint.X;
+                pointMouseDown.Y = formPoint.Y;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (isMaxScreens)
+                {
+                    //isMaxScreens = false;
+                    //this.Width = 188;
+                    //this.Height = 50;
+                    //SetLocation();
+                    //this.BackgroundImage = null;
+                    btnShowAll_Click(null, null);
+                }
+            }
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            mouseLeftDown = false;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseLeftDown = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMaxScreens)
+            {
+                Color color = this.bmp.GetPixel(e.X, e.Y);
+                lblColor.Text = color.R.ToString("X") + " " + color.G.ToString("X") + " " + color.B.ToString("X");
+                pbGetColor.Location = new Point(((-e.X) * 5) + 50, ((-e.Y) * 5) + 50);
+                //Graphics g = this.CreateGraphics();
+                //Graphics g = panel1.CreateGraphics();
+                //Graphics g = pbGetColor.CreateGraphics();
+                //g.FillEllipse(Brushes.Black, panel1.Location.X + 50, panel1.Location.Y + 50, 2, 2);
+                //g.DrawLine(new Pen(Color.Red, 2), new Point(0, 0), new Point(500, 500));
+            }
+            setFormMove();
+        }
+        void setFormMove()
+        {
+            //if (mouseLeftDown)
+            if (MouseButtons.Left == Control.MouseButtons)
+            {
+                Point mouseSet = Control.MousePosition;
+                Point formPoint = this.PointToClient(Control.MousePosition);
+                mouseSet.Offset(mouseOff.X, mouseOff.Y);  //设置移动后的位置  
+                Location = new Point(mouseSet.X - pointMouseDown.X, mouseSet.Y - pointMouseDown.Y);//设置窗体移动后的位置
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            notifyIcon1.Icon = null;
+            notifyIcon1.Dispose();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Color pixelColor = myBitmap.GetPixel(50, 50);
+            //Color color = getScreen().GetPixel(e.X, e.Y);
+            //lblColor.Text = color.R.ToString("X") + " " + color.G.ToString("X") + " " + color.B.ToString("X");
+        }
+        #endregion Form event
+
+        #region btn event
+        /// <summary>
+        /// 获取一个新的guid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGetGuid_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Guid.NewGuid().ToString());
+        }
+        /// <summary>
+        /// 获取DataSet判断非空的if语句
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDataSetNotNull_Click(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+            string rText = @"if (ds != null && ds.Tables != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0) 
+{
+}";
+            if (!string.IsNullOrEmpty(clipboardText))
+            {
+                if (checkDsName(clipboardText)) { Clipboard.SetText(rText.Replace("ds", clipboardText)); }
+            }
+            else
+            {
+                Clipboard.SetText(rText);
+            }
+        }
+        /// <summary>
+        /// 检测是不是为正确的DataSet名称
+        /// </summary>
+        /// <param name="dsName"></param>
+        /// <returns></returns>
+        bool checkDsName(string dsName)
+        {
+            if (string.IsNullOrEmpty(dsName)) { return false; }
+            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(@"^\w*$");
+            return r.IsMatch(dsName);
+        }
+        /// <summary>
+        /// 获取DbHelper查询语句
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGetDbHelper_Click(object sender, EventArgs e)
+        {
+            string rText = @"string sqlStr = """";
+string errorMsg = """";
+DataSet ds = DBHelper.SqlHelper(sqlStr, Constant.DBMenZhen, ref errorMsg);
+if (ds != null && ds.Tables != null && ds.Tables[0].Rows.Count > 0)
+{
+}";
+            Clipboard.SetText(rText);
+        }
+        #endregion btn event
+
+        #region notifyIcon1 event
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (formShow) { this.Hide(); formShow = false; }
+            else { this.Show(); formShow = true; }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (formShow) { this.Hide(); formShow = false; }
+            else { this.Show(); formShow = true; }
+            if (e.Button == MouseButtons.Middle) { this.Close(); Application.Exit(); }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle) { this.Close(); Application.Exit(); }
+        }
+        #endregion notifyIcon1 event
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            if (btnShowAll.Text == "↓")
+            {
+                isMaxScreens = false;
+                btnShowAll.Text = "↑";
+                this.Height = 500;
+                SetLocation();
+            }
+            else
+            {
+                isMaxScreens = false;
+                btnShowAll.Text = "↓";
+                this.Width = 188;
+                this.Height = 23;
+                SetLocation();
+            }
+        }
+        /// <summary>
+        /// 设置贴靠任务栏的位置
+        /// </summary>
+        void SetLocation()
+        {
+            //这个区域不包括任务栏的
+            Rectangle ScreenArea = System.Windows.Forms.Screen.GetWorkingArea(this);
+            //这个区域包括任务栏，就是屏幕显示的物理范围
+            //Rectangle ScreenArea = System.Windows.Forms.Screen.GetBounds(this);
+            this.Location = new System.Drawing.Point(ScreenArea.Width - this.Width, ScreenArea.Height - this.Height); //指定窗体显示在右下角
+        }
+        /// <summary>
+        /// 最大化
+        /// </summary>
+        bool isMaxScreens = false;
+        Bitmap bmp = null;
+        private void btnGetColor_Click(object sender, EventArgs e)
+        {
+            //this.TopMost = false;
+            //this.TopMost = true;
+            if (!isMaxScreens)
+            {
+                isMaxScreens = true;
+                this.Width = Screen.AllScreens[0].Bounds.Size.Width;
+                this.Height = Screen.AllScreens[0].Bounds.Size.Height;
+                this.Location = new Point(0, 0);
+                if (bmp != null)
+                {
+                    bmp.Dispose();
+                }
+                //bmp = null;
+                bmp = getScreen();
+                this.BackgroundImage = bmp;
+                pbGetColor.Width = this.Width * 5;
+                pbGetColor.Height = this.Height * 5;
+                pbGetColor.SizeMode = PictureBoxSizeMode.Zoom;
+
+                pbGetColor.Image = bmp;
+
+                //Graphics g = this.CreateGraphics();
+                ////Graphics g = panel1.CreateGraphics();
+                ////Graphics g = pbGetColor.CreateGraphics();
+                //g.FillEllipse(Brushes.Black, panel1.Location.X + 50, panel1.Location.Y + 50, 2, 2);
+                //g.DrawLine(new Pen(Color.Red,5), new Point(0, 0), new Point(500, 500));
+                //g.dr
+            }
+            else
+            {
+                isMaxScreens = false;
+                this.Width = 188;
+                this.Height = 50;
+                SetLocation();
+                this.BackgroundImage = null;
+            }
+        }
+        /// <summary>
+        /// 屏幕截图函数
+        /// </summary>
+        /// <returns></returns>
+        public static Bitmap getScreen()
+        {
+            Bitmap baseImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(baseImage);
+            g.CopyFromScreen(new Point(0, 0), new Point(0, 0), Screen.AllScreens[0].Bounds.Size);
+            g.Dispose();
+            return baseImage;
+        }
+        /// <summary>
+        /// 16进制转换
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTo16_Click(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(clipboardText))
+            {
+                string[] ctArr = clipboardText.Split(' ');
+                string rText = "";
+                try
+                {
+                    foreach (string ctStr in ctArr)
+                    {
+                        rText += ((byte)int.Parse(ctStr)).ToString("X");
+                    }
+                    Clipboard.SetText(rText);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("转换失败！" + ex.Message);
+                    rText = "";
+                }
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            //如果拖进来的是文件类型
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] paths = e.Data.GetData(DataFormats.FileDrop) as string[];
+                //得到拖进来的路径,取第一个文件
+                string path = paths[0];
+                Clipboard.SetText(path);
+                //路径字符串长度不为空
+                if (path.Length > 1)
+                {
+                    //判断是文件夹吗
+                    FileInfo fil = new FileInfo(path);
+                    if (fil.Attributes == FileAttributes.Directory)//文件夹
+                    {
+                        //鼠标图标链接
+                        e.Effect = DragDropEffects.Link;
+                    }
+                    else//文件
+                    {
+                        //鼠标图标链接
+                        e.Effect = DragDropEffects.Link;
+                    }
+                }
+                else
+                {
+                    //鼠标图标禁止
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+        }
+    }
+}
